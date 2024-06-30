cb58e6fd93f7e042876c904e29bef8ec43a8f548
Sol <darkmaster2210@gmail.com>
2024-06-30 18:38:04 +0200

    Finished quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -22,7 +22,7 @@ pub struct ReportCard<T> {
     pub student_age: u8,
 }
 
-impl<T> ReportCard<T> {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
